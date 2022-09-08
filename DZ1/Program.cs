555a6a65a055548b3ed7260ad0843fa4eba73637
Anastasia <nastys11@gmail.com>
2022-09-08 21:28:24 +0300

    Добавление кода

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -4,9 +4,32 @@ namespace DZ1
 {
     class Program
     {
+        static int vvod ()
+          {
+             Console.WriteLine("Введите пятизначное число:");
+             int number = int.Parse(Console.ReadLine());
+             return number;
+          }
+  
+       static void Polindrom (int number)
+         {
+             int reverse = 0;
+             int numberNotReverse = number;
+             while (numberNotReverse!= 0)
+               {
+                   reverse = 10 * reverse + numberNotReverse % 10;
+                   numberNotReverse = numberNotReverse / 10;
+               }
+             if (number==reverse)
+               {
+                   Console.WriteLine("This is Polindrom!");
+               }
+            else Console.WriteLine("No, this is not Polindrom!");
+         }
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+          {
+              int number = vvod();
+              Polindrom(number);
+          }
     }
 }
