c168904e00c1065d648d4f11439f6f4208e453e7
Anastasia <nastys11@gmail.com>
2022-09-11 19:21:41 +0300

    Code

---
+++ DZ3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DZ3
+{
+    class Program
+    {
+        static int vvod ()
+           {
+              Console.Write("Введите число N: ");
+              int N = int.Parse(Console.ReadLine());
+              return N;
+           }
+        static void vivod(int number)
+          {
+              Console.Write("Таблица кубов от 1 до " + number+": ");
+              for (int i = 1; i <= number;i++)
+              {
+                  Console.Write(Math.Pow(i,3));  
+                  if (i != number)
+                  {
+                      Console.Write(",");  
+                  }
+             else Console.Write(";");  
+              }
+          }
+        static void Main(string[] args)
+        {
+            int number = vvod();
+            vivod(number);
+        }
+    }
+}
